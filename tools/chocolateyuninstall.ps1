40eb277d8c046e4c06151e4834787eb218ee1257
Alan Płócieniak <alan-null@outlook.com>
2018-03-06 22:49:44 +0100

    Added null check for missing registry key

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -2,23 +2,25 @@
     ? { $_.GetValueNames().IndexOf("HelpLink") -gt -1} |
     ? { $_.GetValue("HelpLink").Contains('https://github.com/sitecore/sitecore-instance-manager') } | Select-Object -First 1
 
-$appPathKey = $obj.Name.Replace("HKEY_CURRENT_USER\", "HKCU:\")
-if (Test-Path $appPathKey) {
-    $entry = gi $appPathKey
-    $entry.GetValue("DisplayVersion")
-    $entry.GetValue("DisplayName")
+if ($obj) {
+    $appPathKey = $obj.Name.Replace("HKEY_CURRENT_USER\", "HKCU:\")
+    if (Test-Path $appPathKey) {
+        $entry = gi $appPathKey
+        $entry.GetValue("DisplayVersion")
+        $entry.GetValue("DisplayName")
 
-    $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
-    $ahkExe = 'AutoHotKey'
-    $ahkProcess = "$ahkExe '$scriptPath\sim-uninstall.ahk'"
+        $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+        $ahkExe = 'AutoHotKey'
+        $ahkProcess = "$ahkExe '$scriptPath\sim-uninstall.ahk'"
 
-    $uninst = $entry.GetValue("UninstallString")
-    $index = $uninst.IndexOf(" ")
-    $exe = $uninst.Substring(0, $index)
-    $argz = $uninst.Substring($index, $uninst.Length - $index)
-    Start-Process $exe $argz
-    Start-ChocolateyProcessAsAdmin $ahkProcess
-}
-else {
-    Write-Host $packageName "Nothing to uninstall."
+        $uninst = $entry.GetValue("UninstallString")
+        $index = $uninst.IndexOf(" ")
+        $exe = $uninst.Substring(0, $index)
+        $argz = $uninst.Substring($index, $uninst.Length - $index)
+        Start-Process $exe $argz
+        Start-ChocolateyProcessAsAdmin $ahkProcess
+    }
+    else {
+        Write-Host $packageName "Nothing to uninstall."
+    }
 }
